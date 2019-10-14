8e62a292e6c3d3a544de61af5f051d459fc5d802
jybaek <orangeheim@naver.com>
2019-10-14 16:20:29 +0900

    첫번째커밋

---
+++ blog/app/Http/Controllers/Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public function AddControllerFuntion1()
+    {
+      echo 'test 1';
+    }
+
+
+    public function AddControllerFuntion2()
+    {
+      echo 'test 2';
+    }
+
+
+    public function AddControllerFuntion3()
+    {
+      echo 'test 3';
+    }
+}
+
